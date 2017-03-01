7c9123d9d8be68820ab7b8b3050d8f1d463e3d10
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:36:11 +1100

    First Commit.

---
+++ Loops/Source.cpp
@@ -0,0 +1,299 @@
+// Question 1 A //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 100; i > 0; i--)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 A //
+
+// Question 1 B //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 100; i > -1; i--)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 B //
+
+// Question 1 C //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 99; i > -1; i--)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 C //
+
+// Question 1 D //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 99; i > 0; i--)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 D //
+
+// Question 1 E //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 100; i > -1; i -=2)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 E //
+
+// Question 2 A //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 0; i > 100; i++)
+//	{
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 2 A //
+
+// Question 2 B-D //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	for (int i = 0; i > 100; i++)
+//	{
+//		if (i % 3) // Change to 5 for B. have an or for C.
+//		{
+//			cout << "Fizz";
+//			continue;
+//		}
+//
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 2 B-D //
+
+// Question 3 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int result = 0;
+//
+//	for (int i = 0; i < 1000; i++)
+//	{
+//		if (i % 3 && i % 5)
+//		{
+//			continue;
+//		}
+//
+//		result = result + i;
+//		cout << result << endl;
+//	}
+//
+//	system("pause");
+//}
+// Question 3 //
+
+// Question 4: Do all the exercises instead with using while loops //
+// Question 1 A //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 100;
+//
+//	while (i > 0)
+//	{
+//		i--;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 A //
+
+// Question 1 B //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 100;
+//
+//	while (i > 0)
+//	{
+//		if (i > 99)
+//		{
+//			cout << 100;
+//		}
+//
+//		i--;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 B //
+
+// Question 1 C //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 100;
+//
+//	while (i > 0)
+//	{
+//		i--;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 C //
+
+// Question 1 D //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 100;
+//
+//	while (i > 1)
+//	{
+//		i--;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 D //
+
+// Question 1 E //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 100;
+//
+//	while (i > 0)
+//	{
+//		i-=2;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 1 E //
+
+// Question 2 A //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 0;
+//
+//	while (i < 100)
+//	{
+//		i++;
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 2 A //
+
+// Question 2 B-D //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int i = 0;
+//
+//	while (i < 100)
+//	{
+//		i++;
+//
+//		if (i % 3) // Change to 5 for B. have an or for C.
+//		{
+//			cout << "Fizz";
+//			continue;
+//		}
+//
+//		cout << i;
+//	}
+//
+//	system("pause");
+//}
+// Question 2 B-D //
+
+// Question 3 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int result = 0;
+//	int i = 0;
+//
+//	while (i < 1000)
+//	{
+//		i++;
+//
+//		if (i % 3 && i % 5)
+//		{
+//			continue;
+//		}
+//
+//		result = result + i;
+//		cout << result << endl;
+//	}
+//
+//	system("pause");
+//}
+// Question 3 //
+// Question 4: Do all the exercises instead with using while loops //
